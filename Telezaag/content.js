a00aabd1ddf5e8614c68354d24f3a5dec52a5ce7
meneerkrabs <105109503+meneerkrabs@users.noreply.github.com>
2023-03-22 23:42:27 +0100

    Update content.js
    Added styling

--- Telezaag/content.js
+++ Telezaag/content.js
@@ -1,21 +1,56 @@
 (() => {
   'use strict';
 
+  const styleArticle = (element) => {
+    element.setAttribute('style', 'font-family: "Helvetica Neue", Arial, sans-serif; font-size: 16px; line-height: 1.5em; margin: 0 auto; max-width: 800px; padding: 20px; text-align: justify;');
+  };
+
+  const styleHeader = (element) => {
+    element.style.fontWeight = 'bold';
+  };
+
+  const createArticle = (articleBody) => {
+    const span = document.createElement('span');
+    span.innerHTML = articleBody;
+    styleArticle(span);
+    document.body.appendChild(span);
+  };
+
+  const createImage = (imageUrl) => {
+    const img = document.createElement('img');
+    img.setAttribute('src', imageUrl);
+    img.setAttribute('style', 'display: block; margin: 0 auto; max-width: 800px;');
+    document.body.appendChild(img);
+  };
+
   const displayArticleBodyAndPaytest = () => {
     const scripts = document.querySelectorAll('script[type="application/ld+json"]');
+
+    let articleBody = '';
+    let imageUrl = '';
 
     for (const script of scripts) {
       const json = JSON.parse(script.innerHTML);
 
-      if (json.hasOwnProperty('articleBody')) {
-        const articleBody = json['articleBody'];
-        console.log(articleBody);
-
-        const span = document.createElement('span');
-        span.innerHTML = articleBody;
-
-        document.body.appendChild(span);
+      if (json.articleBody) {
+        articleBody = json.articleBody.replace(/\n\n(.+?)\n\n/g, '<strong>$1</strong>');
       }
+
+      if (json.image) {
+        imageUrl = json.image[0];
+      }
+
+      if (articleBody && imageUrl) {
+        break;
+      }
+    }
+
+    if (articleBody) {
+      createArticle(articleBody);
+    }
+
+    if (imageUrl) {
+      createImage(imageUrl);
     }
 
     const lala = document.getElementById('lala');
@@ -28,27 +63,36 @@
       metering.parentNode.style.display = 'none';
 
       const sc = document.getElementsByTagName('script');
-      let textblock = '';
 
       for (const script of sc) {
         if (script.innerHTML.includes('"@type":"NewsArticle"')) {
-          console.log('Test 1');
           const article = JSON.parse(script.innerHTML);
-          textblock = article.articleBody.replace(/\n/g, '<br>');
+          const textblock = article.articleBody.replace(/\n/g, '<br>');
+          const image = article.image[0];
+
+          if (textblock) {
+            const newp = document.createElement('p');
+            newp.id = 'lala';
+            newp.innerHTML = textblock;
+            styleArticle(newp);
+            const headers = newp.querySelectorAll('h1, h2, h3, h4, h5, h6');
+            headers.forEach(header => {
+              styleHeader(header);
+            });
+            document.getElementsByClassName('TextArticlePage__bodyBlocksContent')[0].appendChild(newp);
+          }
+
+          if (image) {
+            createImage(image);
+          }
           break;
         }
       }
-
-      const newp = document.createElement('p');
-      newp.id = 'lala';
-      newp.innerHTML = textblock;
-      document.getElementsByClassName('TextArticlePage__bodyBlocksContent')[0].appendChild(newp);
     }
   };
 
   document.addEventListener('DOMContentLoaded', () => {
     displayArticleBodyAndPaytest();
-
     const observer = new MutationObserver(displayArticleBodyAndPaytest);
     const observerConfig = { childList: true };
     const observeNode = document.querySelector('title');
